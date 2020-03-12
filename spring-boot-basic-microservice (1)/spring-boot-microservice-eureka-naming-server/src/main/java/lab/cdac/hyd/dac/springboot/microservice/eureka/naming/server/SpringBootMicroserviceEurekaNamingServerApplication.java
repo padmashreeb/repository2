bcb1c1580f78a6ec5794d97637ed70d3375671eb
Admin <Admin@cdac>
2020-03-12 10:17:51 +0530

    new micro serviice project added

---
+++ spring-boot-basic-microservice (1)/spring-boot-microservice-eureka-naming-server/src/main/java/lab/cdac/hyd/dac/springboot/microservice/eureka/naming/server/SpringBootMicroserviceEurekaNamingServerApplication.java
@@ -0,0 +1,16 @@
+package lab.cdac.hyd.dac.springboot.microservice.eureka.naming.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringBootMicroserviceEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceEurekaNamingServerApplication.class, args);
+	}
+}
+//comment
+//comment2
